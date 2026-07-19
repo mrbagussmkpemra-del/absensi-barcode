4da3caa399789700491bcc7a82ab56485a5ddfa8
mrbagussmkpemra-del <mrbagus.smkpemra@gmail.com>
2026-07-19 21:06:40 +0700

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -1,4 +1,59 @@
-alert("scanner.js berhasil dimuat");
+function onScanSuccess(decodedText, decodedResult) {
 
-document.getElementById("hasil").innerHTML =
-"Scanner.js berhasil dijalankan";
+    document.getElementById("hasil").innerHTML =
+    "Mengirim data...";
+
+    html5QrcodeScanner.clear();
+
+    kirimAbsensi(decodedText)
+    .then(function(res){
+
+        if(res.status){
+
+            document.getElementById("hasil").innerHTML=
+            `
+            <h2>✅ ABSENSI BERHASIL</h2>
+
+            <h3>${res.nama}</h3>
+
+            <h3>${res.kelas}</h3>
+            `;
+
+        }else{
+
+            document.getElementById("hasil").innerHTML=
+            `
+            <h2>❌ ${res.pesan}</h2>
+            `;
+
+            setTimeout(function(){
+
+                mulaiScanner();
+
+            },2000);
+
+        }
+
+    });
+
+}
+
+function mulaiScanner(){
+
+    html5QrcodeScanner =
+    new Html5QrcodeScanner(
+        "reader",
+        {
+            fps:10,
+            qrbox:250
+        },
+        false
+    );
+
+    html5QrcodeScanner.render(
+        onScanSuccess
+    );
+
+}
+
+mulaiScanner();
